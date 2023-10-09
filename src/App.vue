37c279bf02165fc28f6d4871bef5b56c68a89489
YuriDonato <yuridonatosis@gmail.com>
2023-10-09 14:32:23 -0300

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,63 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue';
+
+const numero1 = ref(0);
+const numero2 = ref(0);
+const operacao = ref('+');
+
+const resultado = computed(() => {
+  switch (operacao.value) {
+    case '+':
+      return numero1.value + numero2.value;
+    case '-':
+      return numero1.value - numero2.value;
+    case '*':
+      return numero1.value * numero2.value;
+    case '/':
+      return numero1.value / numero2.value;
+    default:
+      return 0;
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <center>
+    <div class="container mt-5 bg-dark-subtle rounded-5">
+      <h1 class="mb-4 pt-4">Calculadora Aritmética</h1>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="numero1">Número 1</label>
+            <input type="number" v-model="numero1" class="form-control">
+          </div>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="numero2">Número 2</label>
+            <input type="number" v-model="numero2" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="operacao">Operação</label>
+            <select v-model="operacao" class="form-control">
+              <option value="+">Adição</option>
+              <option value="-">Subtração</option>
+              <option value="*">Multiplicação</option>
+              <option value="/">Divisão</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3 pb-4">
+        <div class="col-md-12">
+          <h3>Resultado: {{ resultado }}</h3>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </center>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
